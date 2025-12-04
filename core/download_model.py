8ec5889caef5dba7bd4a90250a0d75af168b196b
M11615 <yiyangyan9@gmail.com>
2025-12-04 13:23:16 +0800

    rename

--- core/download_model.py
+++ core/download_model.py
@@ -3,6 +3,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedTokenize
 
 MODEL_ID: str = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 BASE_DIRECTORY: str = os.path.dirname(__file__)
-MODEL_PATH: str = os.path.join(BASE_DIRECTORY, "./.models")
-tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=MODEL_PATH)
-model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(MODEL_ID, cache_dir=MODEL_PATH)
+CACHE_DIRECTORY: str = os.path.join(BASE_DIRECTORY, "./.models")
+tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=CACHE_DIRECTORY)
+model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(MODEL_ID, cache_dir=CACHE_DIRECTORY)
